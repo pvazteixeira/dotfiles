7f08ef6c97b02ddc74a57a3b6d6e68ee7b9be638
pvazteixeira <pvazteixeira@gmail.com>
2014-10-10 00:08:56 -0400

    basic setup script

--- setup.sh
+++ setup.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+CURRENTDIR=$(pwd)
+echo "running script from $CURRENTDIR"
+
+# bash stuff
+for FILE in .{bashrc,bash_aliases,bash_logout,profile,path}
+do
+  if [ -f "$HOME/$FILE" ]; then
+    echo "$FILE exists!"
+  else
+    echo "$FILE does not exist; creating symbolic link."
+    ln -s "$CURRENTDIR/$FILE" "$HOME/$FILE"   #absolute path
+  fi
+done;
+unset FILE
