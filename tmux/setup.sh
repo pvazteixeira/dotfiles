c914f888c048a118848454549e45421abf512943
pvazteixeira <pvazteixeira@gmail.com>
2024-07-10 00:43:25 -0400

    playbook comments, drop tmux setup script

--- tmux/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p "$HOME/.tmux/plugins"
-
-# clone the tmux plugin manager repo
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-# create symlink to user preferences
-ln -s "$HOME/dotfiles/tmux/.tmux.conf" "$HOME/.tmux.conf"
